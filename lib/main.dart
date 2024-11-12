d2491669c5d72c64aceac590565b8180a9ad29c6
Raúl Suescún <raulsuescun@gmail.com>
2024-11-12 18:05:36 -0500

    rename mai app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ModuleTestMainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class ModuleTestMainApp extends StatelessWidget {
+  const ModuleTestMainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Module test'),
         ),
       ),
     );
